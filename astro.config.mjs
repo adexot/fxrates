3e695148ddd03e14fc8cfa0c301a52015635b729
Adekunle Oseni <adekunleoseni47@gmail.com>
2022-08-15 13:55:58 +0200

    feat: add ssr support (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	// support ssr with cloudflare wrangler 
+	output: 'server',
+  adapter: cloudflare()
 });

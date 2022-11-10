b1faf2c4cce9aac253c7c9d2f27e8326aaeff99d
adexot <adekunleoseni47@gmail.com>
2022-11-10 20:44:10 +0000

    fix: update grey endpoint and logic

--- src/services/rates.ts
+++ src/services/rates.ts
@@ -3,7 +3,7 @@ import { fetchUtil } from "../utils/helpers";
 const generateSendUrl = path => `https://sendgateway.myflutterwave.com/api/v1/config/countrypairs/${path}`;
 
 export async function getAllRates(): Promise<any> {
-  const getGreyRates = fetchUtil('https://api.aboki.africa/v2/currency/display');
+  const getGreyRates = fetchUtil('https://user-gw.grey.engineering/v2/currency');
   const getSendEURRate = fetchUtil(generateSendUrl('NL?to=NG'));
   const getSendGBPRate = fetchUtil(generateSendUrl('GB?to=NG'));
   const getSendUSDRate = fetchUtil(generateSendUrl('US?to=NG'));
@@ -12,20 +12,29 @@ export async function getAllRates(): Promise<any> {
     greyRates,
     sendEURRate,
     sendGBPRate,
-    sendUSDRate] = await Promise.all([getGreyRates, getSendEURRate, getSendGBPRate, getSendUSDRate]);
+    sendUSDRate,
+  ] = await Promise.all([
+    getGreyRates,
+    getSendEURRate,
+    getSendGBPRate,
+    getSendUSDRate,
+  ]);
 
   const batchResponse = {
     grey: {},
     send: {}
   }
 
-  greyRates.forEach(element => {
-    batchResponse.grey[element.currency.toLowerCase()] = element.buy_rate
+  greyRates.forEach(item => {
+    const nairaConversions = ['usd-ngn', 'eur-ngn', 'gbp-ngn'];
+    if (nairaConversions.includes(item.code.toLowerCase())) { 
+      batchResponse.grey[item.from_code.toLowerCase()] = item.user_rate;
+    }
   });
 
   batchResponse.send['eur'] = getNGNValueFromSend(sendEURRate['data']);
   batchResponse.send['gbp'] = getNGNValueFromSend(sendGBPRate['data']);
-  batchResponse.send['usd'] = getNGNValueFromSend(sendUSDRate['data']);
+  batchResponse.send['usd'] = getNGNValueFromSend(sendUSDRate['data']); 
 
   return batchResponse;
 }

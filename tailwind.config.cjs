de18c0172a90c4dfb8dc0b4346693ed86486b397
kunle <adekunleoseni47@gmail.com>
2023-11-13 00:20:50 +0100

    feat: add new components for landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			sans: ['"BL Melody"', 'ui-sans-serif','system-ui'],
+		}
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/forms'),
+	],
 };
